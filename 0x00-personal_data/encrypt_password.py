9a532636b1613ac544857ac868075c10d4404415
Oigbochie Blessed <eprince1800@gmail.com>
2024-05-30 16:53:59 +0100

    password encryption task done

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+encryption module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Encrypt password to byte string
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    checks if a password encryption is correct
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
