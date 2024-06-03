68a2ae9b151668b82c9b4eeb6d6614a99c02cc78
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-03 20:10:12 +0100

    initial commit for basic authentication

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -36,6 +36,12 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
     Returns the log message obfuscated
+    args:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is
+            separating all fields in the log line (message)
     """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
