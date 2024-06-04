98f4cd482d2f864b2a7935e68ea465249b98954b
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-04 20:26:20 +0100

    task 7 base 64 part done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,15 @@ class BasicAuth(Auth):
     """
     this class inherits from auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if authorization_header is None or type(
+                                authorization_header) is not str:
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
