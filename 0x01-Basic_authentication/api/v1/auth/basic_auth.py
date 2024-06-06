11c2855402983e3925aa74a701d45ff4b2f54dc9
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-06 16:39:21 +0100

    final fixes for the basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,7 @@
 This module contains the basic auth class
 """
 from api.v1.auth.auth import Auth
-from typing import TypeVar
+from typing import TypeVar, Tuple
 from models.user import User
 import base64
 
@@ -40,7 +40,7 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
         """
         returns the user email and password from the Base64 decoded value
         """
@@ -53,20 +53,23 @@ class BasicAuth(Auth):
         return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+            self, user_email: str, user_pwd: str) -> User:
         """
         returns the User instance based on his email and password
         """
-        if user_email is None or type(user_email) is not str:
-            return None
-        if user_pwd is None or type(user_pwd) is not str:
-            return None
-        try:
-            return User().search({'email': user_email})
-        except Exception:
-            return None
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                print('users is not a list of User objects')
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> User:
         """
         overloads Auth and retrieves the User instance for a request
         """
