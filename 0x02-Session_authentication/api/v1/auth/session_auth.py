eb65ee3acb3f5360985285201951efe0893a0587
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-06 17:20:33 +0100

    initialized session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Authentication module for the app
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    session authentication class
+    for creating a new authentication mechanism.
+    """
+    pass
