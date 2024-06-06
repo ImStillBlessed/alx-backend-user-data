2c2b4800f5ce2a36f7ed18195e206cf6e0201f3c
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-06 16:44:11 +0100

    official initialization for the sesion authentication

--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,9 @@
 This module contains the basic auth class
 """
 from api.v1.auth.auth import Auth
-from typing import TypeVar
+from typing import TypeVar, Tuple
 from models.user import User
+import base64
 
 
 class BasicAuth(Auth):
@@ -33,12 +34,13 @@ class BasicAuth(Auth):
                                 base64_authorization_header) is not str:
             return None
         try:
-            return base64_authorization_header.encode('utf-8').decode('base64')
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
         except Exception:
             return None
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
         """
         returns the user email and password from the Base64 decoded value
         """
@@ -51,20 +53,23 @@ class BasicAuth(Auth):
         return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+            self, user_email: str, user_pwd: str) -> User:
         """
         returns the User instance based on his email and password
         """
-        if user_email is None or type(user_email) is not str:
-            return None
-        if user_pwd is None or type(user_pwd) is not str:
-            return None
-        try:
-            return User().search({'email': user_email})
-        except Exception:
-            return None
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                print('users is not a list of User objects')
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> User:
         """
         overloads Auth and retrieves the User instance for a request
         """
