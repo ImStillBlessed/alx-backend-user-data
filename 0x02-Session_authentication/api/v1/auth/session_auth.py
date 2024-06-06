da6f8330311aa26aa143dd6db53f0566a113cd68
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-06 17:28:05 +0100

    session authorization

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Authentication module for the app
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -10,4 +11,13 @@ class SessionAuth(Auth):
     session authentication class
     for creating a new authentication mechanism.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session ID for the user_id in routing
+        """
+        if type(user_id) == str:
+            session_id = uuid.uuid4()
+            self.user_id_by_session_id[session_id] = user_id
+        return session_id
