0203115ef50f538c389667e970efe0704de47231
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-12 15:55:37 +0100

    task 2 Find user implemented the DB.find_user_by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -39,3 +41,17 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by a given attribute
+        """
+        session = self._session
+        query = session.query(User)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise InvalidRequestError(f"Invalid attribute: {key}")
+            query = query.filter(getattr(User, key) == value)
+        user = query.first()
+        if user is None:
+            raise NoResultFound("No result found for the given filter")
+        return user
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,11 +18,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __init__(self, email: str, hashed_password: str) -> None:
-        """Initialize a new User instance
-        """
-        self.email = email
-        self.hashed_password = hashed_password
-        self.session_id = None
-        self.reset_token = None
