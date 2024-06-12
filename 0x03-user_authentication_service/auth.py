4a5e2fc683e5bec79f86e44424b647f620af40d2
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-12 22:36:49 +0100

    task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,6 +13,9 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+def _generate_uuid():
+    pass
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -30,3 +33,25 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             self._db.add_user(email, _hash_password(password))
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validate login credentials.
+        
+        Args:
+        - email: str - The user's email.
+        - password: str - The user's password.
+        
+        Returns:
+        - bool - True if the login credentials are valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8')):
+                return True
+            return False
+        except NoResultFound:
+            return False
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
