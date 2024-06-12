ee4840e08b4f821298fc402a526134c42162f976
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-12 22:57:40 +0100

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,8 @@
 """
 Flask app module
 """
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
+from flask import make_response, redirect, url_for
 from auth import Auth
 
 
@@ -18,6 +19,7 @@ def flask_home():
     """
     message = {"message": "Bienvenue"}
     return jsonify(message)
+
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
@@ -37,6 +39,53 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions',  methods=['POST'], strict_slashes=False)
+def login():
+    """
+    Log in a user.
+    Expects form data with "email" and "password".
+    If the login is successful, creates a session
+    sets the session ID as a cookie,
+    and returns a JSON response with the login status.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    Log out a user.
+    Expects the session ID as a cookie with key "session_id".
+    If the user exists, destroy the session and redirect to the home page.
+    If the user does not exist, respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get('session_id')
+
+    if not session_id:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect(url_for('flask_home'))
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ method modules for the db setup
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,14 +14,17 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def _generate_uuid():
-    pass
+    """
+    The function should return a string representation of a new UUID
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
     """Auth class to interact with the authentication database.
     """
-
     def __init__(self):
         self._db = DB()
 
@@ -33,21 +37,20 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             self._db.add_user(email, _hash_password(password))
-    
+
     def valid_login(self, email: str, password: str) -> bool:
         """
         Validate login credentials.
-        
         Args:
         - email: str - The user's email.
         - password: str - The user's password.
-        
         Returns:
         - bool - True if the login credentials are valid, False otherwise.
         """
         try:
             user = self._db.find_user_by(email=email)
-            if user and bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8')):
+            if user and bcrypt.checkpw(password.encode('utf-8'),
+                                       user.hashed_password.encode('utf-8')):
                 return True
             return False
         except NoResultFound:
@@ -55,3 +58,46 @@ class Auth:
         except Exception as e:
             print(f"An error occurred: {e}")
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        It takes an email string argument
+        and returns the session ID as a string.
+        """
+        user = self._db.find_user_by(email)
+        session_id = _generate_uuid()
+        user.session_id = session_id
+        self._db._session.commit()
+        return session_id
+
+    def get_user_from_session_id(self, session_id: str):
+        """
+        Retrieve a user based on a session ID.
+        Args:
+        - session_id: str - The session ID.
+        Returns:
+        - User or None: The user if found, otherwise None.
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy a user's session by setting their session ID to None.
+        Args:
+        - user_id: int - The user's ID.
+        Returns:
+        - None
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            user.session_id = None
+            self._db._session.commit()
+        except NoResultFound:
+            pass
